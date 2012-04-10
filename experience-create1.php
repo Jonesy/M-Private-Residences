501f3ca0a1f9f4c31819455aaa277e2d1f253081
Joshua Jones <joshua@general-metrics.com>
2012-04-10 00:41:40 -0700

    Add basic create page.

---
+++ experience-create1.php
@@ -0,0 +1,56 @@
+<?php include('includes/header.php'); ?>
+
+<div id="main" class="grey-page">
+  <div class="container">
+    <section id="experience-creator">
+      <header class="span12">
+        <div class="breadcrumb">
+          <ol>
+            <li class="current">Destination</li>
+            <li>Activities</li>
+            <li>Events</li>
+            <li>Dining</li>
+            <li>Shopping</li>
+            <li>Review & Share</li>
+          </ol>
+        </div> <!-- / .breadcrumb -->
+        <h3>Create your own experience</h3>
+        <h1>I Want to Go To The...</h1>
+        <small>Make your selections below:</small>
+      </header> <!-- / exp creator header -->
+      <ul class="location-selection">
+        <li class="span4">
+          <img src="http://placehold.it/300x148" alt="">
+          <h3><a href="#">Sun <i><img src="images/svg/caret.svg" alt="" class="caret-right"></i></a></h3>
+          <ul>
+            <li><a href="#">Las Vegas, Nevada</a></li>
+            <li><a href="#">New York, New York</a></li>
+            <li><a href="#">St. Remy, Provence, France </a></li>
+            <li><a href="#">London, England</a></li>
+          </ul>
+        </li>
+        <li class="span4">
+          <img src="http://placehold.it/300x148" alt="">
+          <h3><a href="#">Snow <i><img src="images/svg/caret.svg" alt="" class="caret-right"></i></a></h3>
+          <ul>
+            <li><a href="#">Las Vegas, Nevada</a></li>
+            <li><a href="#">New York, New York</a></li>
+            <li><a href="#">St. Remy, Provence, France </a></li>
+            <li><a href="#">London, England</a></li>
+          </ul>
+        </li>
+        <li class="span4">
+          <img src="http://placehold.it/300x148" alt="">
+          <h3><a href="#">City <i><img src="images/svg/caret.svg" alt="" class="caret-right"></i></a></h3>
+          <ul>
+            <li><a href="#">Las Vegas, Nevada</a></li>
+            <li><a href="#">New York, New York</a></li>
+            <li><a href="#">St. Remy, Provence, France </a></li>
+            <li><a href="#">London, England</a></li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+  </div>
+</div>
+<?php include('includes/footer.php'); ?>

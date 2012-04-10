7110840e86226b99d6ceef60be6a4d9f83857045
Joshua Jones <joshua@general-metrics.com>
2012-04-09 23:04:45 -0700

    Add Experience landing page.

---
+++ experience.php
@@ -0,0 +1,65 @@
+<?php include('includes/header.php'); ?>
+
+<section id="experiences" class="full-page">
+  <div class="container">
+    <img src="http://placehold.it/940x440" alt="" style="margin:0 auto">
+  </div>
+  <nav>
+    <ul>
+      <li>
+        <div class="exp-callout">
+          <h2>Experience Name One <i><img src="images/svg/caret.svg" alt="" class="caret-right"></i></h2>
+          <h3>For Couples</h3>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi eu metus in urna tempor tincidunt. Ut vitae diam a ipsum elementum.</p>
+          <div class="exp-thumbnails">
+            <button><img src="images/svg/caret-lrg-left.svg" alt=""></button>
+            <ol>
+              <li><img src="http://placehold.it/80x80" alt=""></li>
+              <li><img src="http://placehold.it/80x80" alt=""></li>
+              <li><img src="http://placehold.it/80x80" alt=""></li>
+            </ol>
+            <button><img src="images/svg/caret-lrg-right.svg" alt=""></button>
+          </div> <!-- / .exp-thumbnails -->
+          <p><a href="#" class="btn btn-silver">Learn More</a></p>
+        </div> <!-- / .exp-callout -->
+      </li>
+      <li>
+        <div class="exp-callout">
+          <h2>Experience Name Two <i><img src="images/svg/caret.svg" alt="" class="caret-right"></i></h2>
+          <h3>Featured Residence</h3>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi eu metus in urna tempor tincidunt. Ut vitae diam a ipsum elementum.</p>
+          <div class="exp-thumbnails">
+            <button><img src="images/svg/caret-lrg-left.svg" alt=""></button>
+            <ol>
+              <li><img src="http://placehold.it/80x80" alt=""></li>
+              <li><img src="http://placehold.it/80x80" alt=""></li>
+              <li><img src="http://placehold.it/80x80" alt=""></li>
+            </ol>
+            <button><img src="images/svg/caret-lrg-right.svg" alt=""></button>
+          </div> <!-- / .exp-thumbnails -->
+          <p><a href="#" class="btn btn-silver">Learn More</a></p>
+        </div> <!-- / .exp-callout -->
+      </li>
+      <li>
+        <div class="exp-callout">
+          <h2>Experience Name Three <i><img src="images/svg/caret.svg" alt="" class="caret-right"></i></h2>
+          <h3>Benefits</h3>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi eu metus in urna tempor tincidunt. Ut vitae diam a ipsum elementum.</p>
+          <div class="exp-thumbnails">
+            <button><img src="images/svg/caret-lrg-left.svg" alt=""></button>
+            <ol>
+              <li><img src="http://placehold.it/80x80" alt=""></li>
+              <li><img src="http://placehold.it/80x80" alt=""></li>
+              <li><img src="http://placehold.it/80x80" alt=""></li>
+            </ol>
+            <button><img src="images/svg/caret-lrg-right.svg" alt=""></button>
+          </div> <!-- / .exp-thumbnails -->
+          <p><a href="#" class="btn btn-silver">Learn More</a></p>
+        </div> <!-- / .exp-callout -->
+      </li>
+    </ul>
+    <div class="shadow"></div>
+  </nav>
+</section> <!-- /#experiences -->
+
+<?php include('includes/footer.php'); ?>

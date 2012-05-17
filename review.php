45abba1073f664d57ee41becbb3e3ea61a80ee67
Joshua Jones <joshua@general-metrics.com>
2012-05-16 23:46:53 -0700

    Add Experience Review page.

---
+++ review.php
@@ -0,0 +1,87 @@
+<?php include('includes/header.php'); ?>
+
+<div id="main" class="grey-page">
+  <div class="container">
+    <section id="experience-creator" class="review">
+      <header class="span12 page-header">
+        <div class="breadcrumb numbered">
+          <ol>
+            <li>Destination</li>
+            <li>Activities</li>
+            <li>Events</li>
+            <li>Dining</li>
+            <li>Shopping</li>
+            <li class="current">Review & Share</li>
+          </ol>
+        </div> <!-- / .breadcrumb -->
+        <div class="review-actions">
+          <a href="#" class="btn btn-white">Print</a>
+          <a href="#" class="btn btn-white">Share</a>
+        </div>
+        <h3>Create your own experience</h3>
+        <h1>Review & Share</h1>
+        <h2>1. New York, NY <a href="#" class="btn btn-white btn-small">Edit</a></h2>
+      </header> <!-- / exp creator header -->
+
+      <div id="experience-review-banner" class="span12">
+        <img src="http://placehold.it/940x340" alt="">
+      </div>
+
+      <div class="review-lists">
+        <div class="span3">
+          <h2>2. Activities</h2>
+          <ul>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Golf Course Name 1</li>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Golf Course Name 2</li>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Tour Name</li>
+          </ul>
+          <a href="#" class="btn btn-white">Add More</a>
+        </div>
+
+        <div class="span3">
+          <h2>3. Events</h2>
+          <ul>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Golf Course Name 1</li>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Golf Course Name 2</li>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Tour Name</li>
+          </ul>
+          <a href="#" class="btn btn-white">Add More</a>
+        </div>
+
+        <div class="span3">
+          <h2>4. Dining</h2>
+          <ul>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Golf Course Name 1</li>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Golf Course Name 2</li>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Tour Name</li>
+          </ul>
+          <a href="#" class="btn btn-white">Add More</a>
+        </div>
+
+        <div class="span3">
+          <h2>5. Shopping</h2>
+          <ul>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Golf Course Name 1</li>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Golf Course Name 2</li>
+            <li><a href="#" class="btn-remove"><img src="/images/svg/button-remove.svg" alt="Remove"></a>Tour Name</li>
+          </ul>
+          <a href="#" class="btn btn-white">Add More</a>
+        </div>
+      </div>
+     
+      <footer>
+        <div class="span6">
+          <a href="#" class="btn-arrow">
+            <h2>Email yourself this experience</h2>
+          </a>
+        </div>
+        <div class="span6">
+          <a href="#" class="btn-arrow">
+            <h2>Share this experience</h2>
+          </a>
+        </div>
+      </footer>
+    </section> <!-- / #experience-creator -->
+  </div>
+</div>
+<?php include('includes/footer.php'); ?>
